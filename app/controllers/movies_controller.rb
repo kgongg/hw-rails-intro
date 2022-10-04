64fff3c9db18352a131d0f5b2dd10822dfc25bc3
Kevin Gong <kevinmgong@berkeley.edu>
2022-10-04 00:32:41 +0000

    ratings_to_show

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,4 @@
 class MoviesController < ApplicationController
-    @all_ratings = Movie.all_ratings
-    @ratings_to_show = Movie.all_ratings
 
     def show
       id = params[:id] # retrieve movie ID from URI route
@@ -11,6 +9,7 @@ class MoviesController < ApplicationController
     def index
       @movies = Movie.all
       @all_ratings = Movie.all_ratings
+      @ratings_to_show = Movie.all_ratings
     end
   
     def new
