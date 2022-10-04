da6bad58e9a8324641610f3363a0157a8cdeda70
Kevin Gong <kevinmgong@berkeley.edu>
2022-10-04 01:03:57 +0000

    finish part 1?

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,9 +7,13 @@ class MoviesController < ApplicationController
     end
   
     def index
-      @movies = Movie.all
       @all_ratings = Movie.all_ratings
-      @ratings_to_show = Movie.all_ratings
+      if params[:ratings].present?
+        @ratings_to_show = params[:ratings].keys
+      else
+        @ratings_to_show = Movie.all_ratings
+      end
+      @movies = Movie.with_ratings(@ratings_to_show)
     end
   
     def new
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -7,5 +7,10 @@ class Movie < ActiveRecord::Base
     # if ratings_list is an array such as ['G', 'PG', 'R'], retrieve all
     #  movies with those ratings
     # if ratings_list is nil, retrieve ALL movies
+    if ratings_list.present?
+      return Movie.where('rating IN (?)', ratings_list)
+    else
+      return Movie.all
     end
+  end
 end
